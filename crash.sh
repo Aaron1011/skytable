7a8c5e1d298b894ac6cde5b239d2115225f03109
Aaron Hill <aa1ronham@gmail.com>
2021-08-09 11:02:09 -0500

    Use bundle instead of test

--- crash.sh
+++ crash.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 git fetch --unshallow
 
 git checkout bb19d024ea1e5e0c9a3d75a9ee58ff03c70c7e5d
-make test
+make bundle
 
 git checkout 11e0cf842628685036265a6164295dd18d543978
 make test
